7331d9b8087db72b0594228308e7d57620341d31
raavinduf <ravinduf@@rocketmail.com>
2020-01-06 22:38:54 +0530

    finished adding functions need to check

--- main.c
+++ main.c
@@ -30,12 +30,29 @@ void insertGame(Games *g,char Name [],int copies){
     np=(ListNode*)malloc(sizeof(ListNode));
     strcpy(np->Name,Name);
     np->copies=copies;
-    g->count++;
-    if(g->head==NULL){
+
+    if(g->count==0){
         g->head=np;
-    }
-    else{
         g->data=np;
+    }
+    g->data->next=np;
+    g->data=np;
+    g->count++;
+
+}
+
+void traverseGames(Games *g){
+    ListNode *np;
+    np=(ListNode*)malloc(sizeof(ListNode));
+    np=g->head;
+    printf("Hello %d",g->count);
+    //printf("%s %d",g->data->Name,g->data->copies);
+    //int i=0;
+    //printf("name of the game=%s no of copies=%d",np->Name,np->copies);
+    while(i<g->count){
+        printf("\n name of the game=%s\t number of copies=%d",np->Name,np->copies);
+        np=np->next;
+        i++;
     }
 }
 
@@ -44,12 +61,14 @@ int insertGameCopies(Games *g,char Name [],int copies){
     ListNode *np;
     np=(ListNode*)malloc(sizeof(ListNode));
     np=g->head;
-    for(i=0;i<copies;i++){
+    for(i=0;i<g->count;i++){
         if(strcmp(np->Name,Name)==0){
-            np->copies=+copies;
+            np->copies=np->copies+copies;
             y=1;
         }
+        np=np->next;
     }
+    i=0;
     if(y){
         return 0;
     }
@@ -58,8 +77,10 @@ int insertGameCopies(Games *g,char Name [],int copies){
         return 0;
     }
 }
+
+
 typedef struct queuenode{
-    int OrderNo;
+    char CustomerName[20];
     char OrderedGame [20];
     struct queuenode *next;
 }QueueNode;
@@ -83,20 +104,80 @@ int isOrdersEmpty(Orders *o){
     return 0;
 }
 
-int insertOrder(Orders *o,int Order_number,char Orderedgame[] ){
+int insertOrder(Orders *o,char Customer_name[],char Ordered_game[] ){
     QueueNode *np;
     np=(QueueNode*)malloc(sizeof(QueueNode));
-    np->OrderNo=Order_number;
-    strcpy(np->OrderedGame,Orderedgame);
+    strcpy(np->CustomerName,Customer_name);
+    strcpy(np->OrderedGame,Ordered_game);
     if(isOrdersEmpty(o)){
         o->rear=np;
-        o->head=np;
+        o->front=np;
         o->count++;
     }
     //np->next=o->rear;
     o->rear->next=np;
-    0->rear=np;
+    o->rear=np;
     o->count++;
     return 0;
+}
+
+
+
+typedef struct stacknode{
+    char game [20];
+    char customer [20];
+    struct stacknode *next;
+}StackNode;
+
+typedef struct SoldGames{
+    StackNode *top;
+    int no_of_sold_games;
+}SGames;
+
+int createSoldGamesStack(SGames *s){
+    s->top=NULL;
+    s->no_of_sold_games=0;
+    return 0;
+}
+
+int isSoldGamesEmpty(SGames *s){
+    if(s->top==NULL){
+        return 1;
+    }
+    return 0;
+}
+
+int pushSGames(SGames *s,char g [20],char customer [20]){
+    StackNode *np;
+    np=(StackNode*)malloc(sizeof(StackNode));
+    strcpy(np->customer,customer);
+    strcpy(np->game,g);
+    np->next=s->top;
+    s->top=np;
+    s->no_of_sold_games++;
+    return 0;
+}
+
+
+int orderComplete(SGames *s,Orders *o){
+    if(isSoldGamesEmpty(s)){
+        createSoldGamesStack(s);
+    }
+    pushSGames(s,o->front->OrderedGame,o->front->CustomerName);
+
+}
+
+int main(){
+    Games g; Orders o; SGames s;
+    instializeGames(&g);
+    insertGame(&g,"hi",20);
+    insertGame(&g,"hello",13);
+    insertGame(&g,"Forza",15);
+    traverseGames(&g);
+    insertGameCopies(&g,"hello",10);
+    traverseGames(&g);
+    createOrders(&o);
+    insertOrder(&o,"Ravindu","Forza");
+
 }
 
